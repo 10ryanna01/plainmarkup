715884fa80517812626e589fb8f39f7c76e59924
Ryann Al-lahiq <ryann.allahiq@gmail.com>
2017-08-15 21:42:51 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # plainmarkup
 framework free semantics
+
+
+basic sass partials with BEM styling 
+basic break points for testing
+
+basic static markup for testing responsive
---
+++ app/scripts/main.js
@@ -0,0 +1,15 @@
+/// ====================================
+/// custom js/jq go here
+// ====================================
+$(document).ready(function () {
+	console.log('rocknroll here we go!'); 
+ 
+ 
+ 
+//end document load event
+		});
+
+ 
+
+	
+ 
